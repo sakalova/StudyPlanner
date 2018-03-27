d539d2fa83893f633b6afac61474102f8332e7f1
Mariya <34890275+BSroad@users.noreply.github.com>
2018-03-27 13:27:01 +0300

    Update frontend_script.py

--- frontend_script.py
+++ frontend_script.py
@@ -9,59 +9,63 @@ window.resizable(width=FALSE, height=FALSE)
 
 
 class LabelEnter:
-    """ """
+    """ Class for creation Label and Entry widgets."""
+
     def __init__(self):
-        self.lbl_enter = Label(window, text="Enter what do you plan to learn:", font=("Times", "14", "bold italic"),) # Enter what you plan to learn LABEL
-        self.lbl_enter.grid(row=0, column=0, columnspan=3,)
+        self.lbl_enter = Label(window, text="Enter what do you plan to learn:",
+                               font=("Times", "14", "bold italic"), )  # Enter what you plan to learn LABEL
+        self.lbl_enter.grid(row=0, column=0, columnspan=3, )
         self.enter_text = StringVar()
 
-        self.entry_enter = Entry(window, textvariable=self.enter_text, width=50, cursor='arrow', fg='dark blue', font=("Times", "12", "italic"), )
+        self.entry_enter = Entry(window, textvariable=self.enter_text, width=50, cursor='arrow', fg='dark blue',
+                                 font=("Times", "12", "italic"), )
         self.entry_enter.grid(row=1, column=0, columnspan=3, ipady=4)
 
 
 class Buttons:
-    """ """
+    """ Class for creation Button widget and command modules for buttons."""
+
     def __init__(self):
         # Create buttons for GUI and grid
         # Create button 'Add'
         self.button_add = Button(window, text='Add', width=11, activebackground='white', relief=RIDGE,
-                            font=("Times", "14", "italic"), cursor='hand2', command=self.add_command)
+                                 font=("Times", "14", "italic"), cursor='hand2', command=self.add_command)
         self.button_add.grid(row=1, column=4, padx=5, pady=5)
 
         # Create button 'My plan'
         self.button_plan = Button(window, text='My plan', width=12, activebackground='white', relief=GROOVE,
-                             font=("Times", "14", "bold italic"), cursor='hand2', command=self.view_command)
+                                  font=("Times", "14", "bold italic"), cursor='hand2', command=self.view_command)
         self.button_plan.grid(row=2, column=0, padx=3, pady=5)
 
         # Create button 'In progress'
         self.button_progr = Button(window, text='In progress', width=12, activebackground='white', relief=GROOVE,
-                              font=("Times", "14", "bold italic"), cursor='hand2', command=self.view_p_command)
+                                   font=("Times", "14", "bold italic"), cursor='hand2', command=self.view_p_command)
         self.button_progr.grid(row=2, column=1, padx=3, pady=5)
 
         # Create button 'Finished'
         self.button_finish = Button(window, text='Finished', width=12, activebackground='white', relief=GROOVE,
-                               font=("Times", "14", "bold italic"), cursor='hand2', command=self.view_f_command)
+                                    font=("Times", "14", "bold italic"), cursor='hand2', command=self.view_f_command)
         self.button_finish.grid(row=2, column=2, padx=5, pady=5)
-
 
         # Create button 'Update'
         self.button_update = Button(window, text='Update', width=12, activebackground='white', relief=RIDGE,
-                               font=("Times", "12", "italic"), cursor='hand2', command=self.update_command)
+                                    font=("Times", "12", "italic"), cursor='hand2', command=self.update_command)
         self.button_update.grid(row=6, column=1, padx=5, pady=5)
 
         # Create button 'Delete'
         self.button_delete = Button(window, text='Delete', width=12, activebackground='white', relief=RIDGE,
-                               font=("Times", "12", "italic"), cursor='hand2', command=self.delete_command)
+                                    font=("Times", "12", "italic"), cursor='hand2', command=self.delete_command)
         self.button_delete.grid(row=6, column=2, padx=5, pady=5)
 
         # Create button 'Push to  In progress'
         self.button_push_prog = Button(window, text='Push to   In progress', width=12, height=3, wraplength=75,
                                        justify=CENTER, activebackground='white', relief=RIDGE,
-                                       font=("Times", "12", "italic"), cursor='hand2', command = self.push_to_progress)
+                                       font=("Times", "12", "italic"), cursor='hand2', command=self.push_to_progress)
         self.button_push_prog.grid(row=3, column=4, padx=5, pady=5)
 
         # Create button 'Push to Finished'
-        self.button_push_fin = Button(window, text='Push to Finished', width=12, height=3, wraplength=75, justify=CENTER,
+        self.button_push_fin = Button(window, text='Push to Finished', width=12, height=3, wraplength=75,
+                                      justify=CENTER,
                                       activebackground='white', relief=RIDGE, font=("Times", "12", "italic"),
                                       cursor='hand2', command=self.push_to_fin)
         self.button_push_fin.grid(row=4, column=4, padx=5, pady=5)
@@ -85,21 +89,24 @@ class Buttons:
         print(type(label_enter.enter_text.get()))
         list_scrollbar.list.insert(END, label_enter.enter_text.get())
 
+    # When push button 'My plan' in the listbox are shown entered text with status==0 (status 0 - means status 'my plan').
     def view_command(self):
         list_scrollbar.list.delete(0, END)
         for row in database.view():
             list_scrollbar.list.insert(END, row)
 
+    # When push button 'In progress' in the listbox are shown entered text with status==1 (status 1 - means status 'in progress').
     def view_p_command(self):
         list_scrollbar.list.delete(0, END)
         for row in database.view_p():
             list_scrollbar.list.insert(END, row)
 
+    # When push button 'Finished' in the listbox are shown entered text with status==2 (status 2 - means status 'finished').
     def view_f_command(self):
         list_scrollbar.list.delete(0, END)
         for row in database.view_f():
             list_scrollbar.list.insert(END, row)
-            
+
     def delete_command(self):
         try:
             database.delete(selected_tuple[0])
@@ -114,11 +121,13 @@ class Buttons:
             pass
         self.view_command()
 
+
 class ListScrollbar:
-    """ """
+    """Class for creation Listbox and Scrollbar widget and  connection of vertical scrollbar to listbox. """
+
     def __init__(self):
         self.list = Listbox(window, width=60, fg='dark green', font=("Times", "12", "italic"), selectmode=SINGLE, )
-        self.list.grid(row=3, column=0, rowspan=2, columnspan=3, padx=5,)
+        self.list.grid(row=3, column=0, rowspan=2, columnspan=3, padx=5, )
 
         self.scrollbar_list = Scrollbar(window, cursor='double_arrow', )
         self.scrollbar_list.grid(row=3, column=3, rowspan=3, ipady=45, padx=3, pady=3)
